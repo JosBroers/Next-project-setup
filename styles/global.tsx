b8b1d12948053a39d852b0d249b66bd91433f008
Jos Broers <josdab.broers@gmail.com>
2020-11-30 18:05:09 +0100

    Fix scroll top

--- styles/global.tsx
+++ styles/global.tsx
@@ -5,13 +5,13 @@ const Global = createGlobalStyle`
   body {
     color: var(--color-black);
     background-color: var(--color-white);
+    scroll-behavior: smooth;
   }
 
   body {
     font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     font-size: 18px;
     line-height: 1.5rem;
-    scroll-behavior: smooth;
     overflow-x: hidden;
   }
 
