4c57f82742688fd284360bcdc6574fe499185299
Jos Broers <josdab.broers@gmail.com>
2020-11-30 11:46:32 +0100

    Adding page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { AppProps } from "next/app"
 import { NextSeo } from "next-seo"
+import { AnimatePresence, motion } from "framer-motion"
 
 import "../fonts/inter.css"
 import GlobalStyle from "../styles/index"
@@ -49,7 +50,31 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
         }}
       />
       <GlobalStyle />
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          exit="pageExit"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+              transition: {
+                velocity: 100,
+                type: "Inertia",
+              },
+            },
+            pageExit: {
+              opacity: 0,
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
