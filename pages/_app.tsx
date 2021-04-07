0501ac62e0c7c4567d88c38d4b9fbbbdc77af1de
Jos Broers <josdab.broers@gmail.com>
2021-04-07 19:32:33 +0200

    React conventions and optimizations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,18 @@
 import React from "react"
-import type { AppProps } from "next/app"
+import type {AppProps} from "next/app"
 import Head from "next/head"
 
 // CSS Imports
-import "normalize.css/normalize.css"
-import "../fonts.css"
-import "../styles/global.scss"
+import "@styles/main.scss"
 
-export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/images/favicon.png" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+export default function MyApp({Component, pageProps}: AppProps) {
+	return (
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/images/favicon.png" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,26 @@ import React from "react"
 import Head from "next/head"
 
 // Component Imports
-import Container from "../components/container"
+import Container from "@components/Container"
 
-export default function Home(): JSX.Element {
-  return (
-    <>
-      <Head>
-        <title>Homepage | Next Project Setup</title>
-        <meta
-          name="description"
-          content="This repository is a setup project which can be used for starting an application with
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Homepage | Next Project Setup</title>
+				<meta
+					name="description"
+					content="This repository is a setup project which can be used for starting an application with
           Next.js."
-        />
-      </Head>
-      <Container>
-        <h1>Next.js Project Setup</h1>
-        <p>
-          This repository is a setup project which can be used for starting an application with
-          Next.js.
-        </p>
-      </Container>
-    </>
-  )
+				/>
+			</Head>
+			<Container>
+				<h1>Next.js Project Setup</h1>
+				<p>
+					This repository is a setup project which can be used for starting an application with
+					Next.js.
+				</p>
+			</Container>
+		</>
+	)
 }
