d8953a20ddfcb49bd1c6fd9d1ae2c7bade024175
Jos Broers <jos@jobrodo.nl>
2021-04-26 21:10:22 +0200

    Version 0.3.0 (#18)
    * Feature: Line clamping (#9)
    
    * Adding line-clamp functionality
    
    * Reformat code
    
    * Adding CodeQL Analysis to repo (#10)
    
    * Draft: Feature/flexboxgrid (#12)
    
    * Adding flexboxgrid
    
    * WIP: Adding ESlint, Stylelint and flexboxgrid
    
    * Fixing indentations
    
    * WIP
    
    * Adding reflexbox and styled-components
    
    * Adding css breakpoint variables
    
    Co-authored-by: Jos Broers <josdab.broers@gmail.com>
    
    * Bug: Transform line clamping into component (#13)
    
    * Adding component for line clamping
    
    * Finish up
    
    * Update stylelint-config-standard
    
    * Fixing TypeScript error
    
    Co-authored-by: Jos Broers <josdab.broers@gmail.com>
    
    * Renaming files (#14)
    
    Co-authored-by: Jos Broers <josdab.broers@gmail.com>
    
    * Feature: Imagekit.io (#15)
    
    * Feature: NProgress (#16)
    
    * Bug: Deleting .env file (#17)
    
    Co-authored-by: Jos Broers <josdab.broers@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useEffect } from "react"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { ThemeProvider } from "styled-components"
+import NProgress from "nprogress"
 
 // CSS
 import "../styles/main.scss"
@@ -10,14 +11,43 @@ const theme = {
 	breakpoints: ["576px", "768px", "992px", "1024px"],
 }
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-	<ThemeProvider theme={theme}>
-		<Head>
-			<link rel="shortcut icon" href="/images/favicon.png" />
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		</Head>
-		<Component {...pageProps} />
-	</ThemeProvider>
-)
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
+	const url = process.env.NEXT_PUBLIC_SITE_URL + router.pathname
+
+	useEffect(() => {
+		const routeChangeStart = () => NProgress.start()
+		const routeChangeComplete = () => NProgress.done()
+
+		router.events.on("routeChangeStart", routeChangeStart)
+		router.events.on("routeChangeComplete", routeChangeComplete)
+		router.events.on("routeChangeError", routeChangeComplete)
+
+		return () => {
+			router.events.off("routeChangeStart", routeChangeStart)
+			router.events.off("routeChangeComplete", routeChangeComplete)
+			router.events.off("routeChangeError", routeChangeComplete)
+		}
+	}, [])
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Head>
+				<link
+					rel="shortcut icon"
+					type="image/png"
+					href="https://ik.imagekit.io/jobrodo/jobrodo/favicon_UMYJ6XtNm.png"
+				/>
+				<link rel="canonical" href={url} />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta property="og:url" content={url} />
+				<meta name="robots" content="index, follow" />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
+				<meta name="theme-color" content="#000" />
+			</Head>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
+}
 
 export default MyApp
