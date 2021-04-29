1a56a0a551b70e885c58b6cf3e1a78b719ab8eb3
Jos Broers <jos@jobrodo.nl>
2021-04-29 21:45:21 +0200

    NPS-8: Fixing error on property dataLayer

--- lib/gtm.ts
+++ lib/gtm.ts
@@ -1,5 +1,11 @@
 export const GTM_ID = process.env.NEXT_PUBLIC_GTM
 
+declare global {
+	interface Window {
+		dataLayer: any
+	}
+}
+
 export const pageview = url => {
 	window.dataLayer.push({
 		event: "pageview",
