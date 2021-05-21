d958b566f4179984dbd5154d68710423c16d15cb
Jos Broers <jos@jobrodo.nl>
2021-05-21 18:13:28 +0200

    #35: Removing page transitions (#50)
    * Revert "#35: Adding page transitions (#49)"
    
    This reverts commit 2cdd37b95a3073a7afa86637f37f6c949048e771.
    
    * Upgrade packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import { ThemeProvider } from "styled-components"
 import NProgress from "nprogress"
-import { PageTransition } from "next-page-transitions"
 
 // CSS imports
 import "../styles/main.scss"
@@ -36,28 +35,26 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
 	}, [])
 
 	return (
-		<PageTransition timeout={400} classNames="page-transition">
-			<GoogleTagManager>
-				<ThemeProvider theme={theme}>
-					<Head>
-						<link
-							rel="shortcut icon"
-							type="image/png"
-							href="https://ik.imagekit.io/jobrodo/jobrodo/favicon_UMYJ6XtNm.png"
-						/>
-						<link rel="canonical" href={url} />
-						<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-						<meta property="og:url" content={url} />
-						<meta name="robots" content="index, follow" />
-						<meta property="og:type" content="website" />
-						<meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
-						<meta name="theme-color" content="#000" />
-					</Head>
-					<CookieNotice />
-					<Component {...pageProps} key={router.route} />
-				</ThemeProvider>
-			</GoogleTagManager>
-		</PageTransition>
+		<GoogleTagManager>
+			<ThemeProvider theme={theme}>
+				<Head>
+					<link
+						rel="shortcut icon"
+						type="image/png"
+						href="https://ik.imagekit.io/jobrodo/jobrodo/favicon_UMYJ6XtNm.png"
+					/>
+					<link rel="canonical" href={url} />
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					<meta property="og:url" content={url} />
+					<meta name="robots" content="index, follow" />
+					<meta property="og:type" content="website" />
+					<meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
+					<meta name="theme-color" content="#000" />
+				</Head>
+				<CookieNotice />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</GoogleTagManager>
 	)
 }
 
