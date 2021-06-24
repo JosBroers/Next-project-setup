b59f63929b3df13c415df15399028c427c24a061
Jos Broers <jos.broers@indicia.nl>
2021-06-24 22:33:29 +0200

    #94: Adding preact

--- next.config.js
+++ next.config.js
@@ -87,4 +87,15 @@ module.exports = withBundleAnalyzer({
 			},
 		]
 	},
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: "preact/compat",
+				"react-dom/test-utils": "preact/test-utils",
+				"react-dom": "preact/compat",
+			})
+		}
+
+		return config
+	},
 })
